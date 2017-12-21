b0bd881871b3079fbef42529397c9c436c854390
Vu Le <Nguyen.Le38@T-mobile.com>
2017-12-21 16:35:20 -0600

    change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// This is main application. adasdadassddsfsdfasdasdas
+		// This is main application. adasdadassddsfsdfasdasdasdasdadas
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
